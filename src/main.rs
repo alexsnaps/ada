43bb2c73ea0b1a99d3e86815aa2267b562c521c8
dd di cesare <didi@posteo.net>
2025-09-10 19:03:44 +0200

    TRL like a MOFO
    Signed-off-by: dd di cesare <didi@posteo.net>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use crate::PendingValue::Revolved;
 use crate::Phase::ResponseHeaders;
 use std::cell::RefCell;
-use std::{collections::BTreeMap, ops::Not, rc::Rc};
+use std::{collections::BTreeMap, rc::Rc};
 
 enum Either<T, U> {
     Left(T),
@@ -11,8 +11,8 @@ enum Either<T, U> {
 struct Service {}
 
 impl Service {
-    pub(crate) fn dispatch(&self, _ctx: &mut ReqRespCtx) -> usize {
-        0
+    pub(crate) fn dispatch(&self, ctx: &mut ReqRespCtx) -> usize {
+        ctx.next_token_id()
     }
     fn parse_message(&self, mut message: Vec<u8>) -> bool {
         Some(1u8) == message.pop()
@@ -37,6 +37,9 @@ impl Pipeline {
             match either {
                 Either::Left(None) => {}
                 Either::Left(Some((token_id, t))) => {
+                    if self.pending_tasks.contains_key(&token_id) {
+                        panic!("NONONONONO")
+                    }
                     self.pending_tasks.insert(token_id, t);
                 }
                 Either::Right(todo) => {
@@ -56,11 +59,13 @@ impl Pipeline {
     fn digest(&mut self, token_id: usize, response: Vec<u8>) {
         if let Some(pending) = self.pending_tasks.remove(&token_id) {
             // Process the response
-            if let Some(action) = pending.process_response(response) {
-                if !action.apply(&mut self.ctx) {
-                    self.pending_actions.push(action);
-                }
+            if let Some(action) = pending.process_response(response)
+                && !action.apply(&mut self.ctx)
+            {
+                self.pending_actions.push(action);
             };
+        } else {
+            panic!("token_id={} not found", token_id);
         }
     }
 
@@ -69,11 +74,12 @@ impl Pipeline {
     }
 }
 
+#[cfg(test)]
 impl Drop for Pipeline {
     fn drop(&mut self) {
-        if self.todos.is_empty().not()
-            || self.pending_tasks.is_empty().not()
-            || self.pending_actions.is_empty().not()
+        if !self.todos.is_empty()
+            || !self.pending_tasks.is_empty()
+            || !self.pending_actions.is_empty()
         {
             panic!("Pipeline dropped with pending tasks");
         }
@@ -89,7 +95,7 @@ struct RLTask {
 
 impl RLTask {
     fn exec(self, ctx: &mut ReqRespCtx) -> Either<Option<(usize, PendingTask)>, Self> {
-        match self.predicate.eval() {
+        match self.predicate.eval(ctx) {
             Revolved(exec) => {
                 if exec {
                     let token_id: usize = self.service.dispatch(ctx);
@@ -171,39 +177,49 @@ enum PendingValue<T> {
 struct Predicate {}
 
 impl Predicate {
-    fn eval(&self) -> PendingValue<bool> {
-        Revolved(true)
+    fn eval(&self, ctx: &mut ReqRespCtx) -> PendingValue<bool> {
+        ctx.test_pop_predicate_value()
     }
 }
 
 #[derive(Eq, PartialEq)]
 enum Phase {
     RequestHeaders,
-    RequestBoby,
+    RequestBody,
     ResponseHeaders,
-    ResponseBoby,
+    ResponseBody,
 }
 
 #[derive(Default)]
 struct ReqRespCtx {
+    test_token_id: usize,
     test_current_phase: Rc<RefCell<Option<Phase>>>,
+    test_predicate_values: Vec<PendingValue<bool>>,
     status_code: Option<u32>,
     response_headers: Vec<(String, String)>,
 }
 
 impl ReqRespCtx {
+    fn test_pop_predicate_value(&mut self) -> PendingValue<bool> {
+        self.test_predicate_values.pop().expect("Expected a value")
+    }
     fn add_response_header(&mut self, key: &str, value: &str) {}
+    fn next_token_id(&mut self) -> usize {
+        self.test_token_id += 1;
+        self.test_token_id
+    }
 }
 
-#[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn it_rate_limits() {
         // on_request_headers() {
+        let mut ctx = ReqRespCtx::default();
+        ctx.test_predicate_values.push(PendingValue::Revolved(true));
         let mut pipeline = Pipeline {
-            ctx: Default::default(),
+            ctx,
             todos: vec![RLTask {
                 predicate: Predicate {},
                 service: Service {}.into(),
@@ -221,7 +237,7 @@ mod tests {
 
         // fn on_grpc_call_response(&mut self, token_id: u32, status_code: u32, resp_size: usize) {
         let buffer: Vec<u8> = vec![1u8];
-        let token_id = 0;
+        let token_id = 1;
         pipeline.digest(token_id, buffer);
         assert!(!pipeline.is_blocked(), "Filter should be continued");
         assert_eq!(pipeline.ctx.status_code, Some(429));
@@ -240,6 +256,7 @@ mod tests {
         let mut ctx = ReqRespCtx::default();
         let mut rc = Rc::new(RefCell::new(Some(Phase::RequestHeaders)));
         ctx.test_current_phase = rc.clone();
+        ctx.test_predicate_values.push(PendingValue::Revolved(true));
         let mut pipeline = Pipeline {
             ctx: ctx,
             todos: vec![RLTask {
@@ -261,7 +278,7 @@ mod tests {
 
         // fn on_grpc_call_response(&mut self, token_id: u32, status_code: u32, resp_size: usize) {
         let buffer: Vec<u8> = Vec::new();
-        let token_id = 0;
+        let token_id = 1;
         pipeline.digest(token_id, buffer);
         assert!(!pipeline.is_blocked(), "Filter should be continued");
         assert_eq!(pipeline.ctx.status_code, None);
@@ -287,6 +304,68 @@ mod tests {
 
         // on_response_body() {
         // pipeline.eval();
+    }
+
+    #[test]
+    fn it_token_rate_limits() {
+        // on_request_headers() {
+        let mut ctx = ReqRespCtx::default();
+        let mut rc = Rc::new(RefCell::new(Some(Phase::RequestHeaders)));
+        ctx.test_current_phase = rc.clone();
+        ctx.test_predicate_values
+            .insert(0, PendingValue::Revolved(true));
+        ctx.test_predicate_values.insert(0, PendingValue::Pending);
+        ctx.test_predicate_values.insert(0, PendingValue::Pending);
+        ctx.test_predicate_values.insert(0, PendingValue::Pending);
+        ctx.test_predicate_values
+            .insert(0, PendingValue::Revolved(true));
+        let mut pipeline = Pipeline {
+            ctx: ctx,
+            todos: vec![
+                RLTask {
+                    predicate: Predicate {},
+                    service: Service {}.into(),
+                    ok_action: None,
+                    rl_action: Box::new(TooManyRequestsAction {}),
+                },
+                RLTask {
+                    predicate: Predicate {},
+                    service: Service {}.into(),
+                    ok_action: None,
+                    rl_action: Box::new(TooManyRequestsAction {}),
+                },
+            ],
+            pending_tasks: Default::default(),
+            pending_actions: Default::default(),
+        };
+
+        pipeline = pipeline
+            .eval()
+            .expect("Pipeline should be waiting for limitador");
+        assert!(pipeline.is_blocked(), "Filter should be paused");
+
+        // fn on_grpc_call_response(&mut self, token_id: u32, status_code: u32, resp_size: usize) {
+        let buffer: Vec<u8> = Vec::new();
+        let token_id = 1;
+        pipeline.digest(token_id, buffer);
+        assert!(!pipeline.is_blocked(), "Filter should be continued");
+        assert_eq!(pipeline.ctx.status_code, None);
+
+        // on_request_body() {
+        pipeline = pipeline.eval().expect("Not done yet");
+
+        // on_response_headers() {
+        pipeline = pipeline.eval().expect("Not done yet");
+        // assert_eq!(
+        //     pipeline.ctx.headers,
+        //     vec![("x".to_string(), "y".to_string())]
+        // );
+
+        // on_response_body() {
+        pipeline = pipeline.eval().expect("Not done yet");
+
+        // on_grpc_response
+        pipeline.digest(2, vec![1u8]);
     }
 }
 
